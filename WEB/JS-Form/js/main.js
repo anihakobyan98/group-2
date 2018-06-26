ce05c123647915c79b70cd5d2c899223be18ba95
Ani <ani_hakobyan_98@bk.ru>
2018-06-26 09:35:16 +0400

    Form JS

--- WEB/JS-Form/js/main.js
+++ WEB/JS-Form/js/main.js
@@ -1,33 +1,55 @@
-//  function sum() 
-// { 
-//     var fn, ln; 
-//     fn = new Number(document.getElementById("n1").value); 
-//     ln = new Number(document.getElementById("n2").value); 
-//     result =  (fn+ln); 
-//     document.getElementById("demo2").innerHTML = result; 
-// }
 
-function newpage() {
-    var x = document.getElementById("form1");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+// function newpage() {
+//     var x = document.getElementById("form1");
+//     if (x.style.display === "block") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "block";
+//     }
      
+// }
+jQuery(document).ready(function($) {
+    $("#myModal").modal('show');
+  });
+var count = 0;
+function newpage() {  
+    if(document.getElementById('male').checked) {
+        $("#info").html("Mr.");
+    } 
+    if(document.getElementById('female').checked) {
+        $("#info").html("Mrs.");
+    }
+    $("#info").append(" " + $("#name").val() + " ");
+    $("#info").append($("#lastname").val());
 }
-
-function myFunction() {
-    //x = document.getElementById("name").value;
-    Name = document.getElementsByTagName("input")[0].value
-    document.getElementById("demo").innerHTML = "Name: " + Name;
-    LastName = document.getElementsByTagName("input")[1].value
-    document.getElementById("log").innerHTML = "LastName: " + LastName;
-    Phone = document.getElementsByTagName("input")[2].value
-    document.getElementById("numb").innerHTML = "Phone: " + Phone;
-    Country = document.getElementsByTagName("input")[3].value
-    document.getElementById("country").innerHTML = "Country: " + Country;
-    City = document.getElementsByTagName("input")[4].value
-    document.getElementById("city").innerHTML = "City: " + City;
-}
+ function addFunction() {
+     var add = "Address: " + $("#address").val();
+     var cty = "City: " + $("#city").val(); 
+     var co  = "Country: " + $("#country").val(); 
+     var ph  = "Phone: " + $("#phone").val(); 
+     var nm  = "Name: " + $("#name1").val(); 
+     var snm = "Lastname: " + $("#lastname1").val();       
+     var arr = [add,cty,co,ph,nm,snm];
+        var content = "<table>"
+         for(i=0; i<6; i++){
+             content += '<tr><td>' + arr[i] + '</td></tr>';
+         }
+         content += "</table>"
+         $('#area').append(content); 
+     $("#form2")[0].reset();
+     }
+// window.onload = function saveSettings() {
+//     var name = document.getElementById("name").value;
+//     var lastname = document.getElementById("lastname").value;
+//     var gender = document.getElementById("gender").value;
+//     if (document.getElementById("lastname").checked) {
+//         gender = "Mister";
+//     } else if(document.getElementById("lastname").checked) {
+//         gender = "Misis";
+//     }
+//     ​var test = { name: name, lastname: lastname, gender: gender }​​​​​​​;
+//     localStorage.setItem("test", JSON.stringify(test));
+//     var test2 = localStorage.getItem("test");
+//     test = JSON.parse(test2);
+// }
 
